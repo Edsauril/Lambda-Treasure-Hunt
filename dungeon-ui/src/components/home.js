f272852dc6f1cbfe9e5dae445f9455b1fe4998e8
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-02-05 13:40:02 -0500

    local storage works, and is set to state

--- dungeon-ui/src/components/home.js
+++ dungeon-ui/src/components/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 const elements = ["one", "two", "three"];
 
@@ -6,13 +6,35 @@ const rooms = [];
 elements.forEach(function(element) {
   rooms.push(<p>{element}</p>);
 });
-const Home = props => {
-  return (
-    <div className="home">
-      <h1>MAP</h1>
-      {rooms}
-    </div>
-  );
-};
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      localStorage: ""
+    };
+  }
+  componentDidMount() {
+    let message = localStorage.getItem("message");
+    this.setState({ localStorage: message });
+  }
+  clickHandler = event => {
+    event.preventDefault();
+    alert("This button works, but doesnt do anything yet");
+
+    localStorage.setItem("message", "we set local storage");
+  };
+
+  render() {
+    return (
+      <div className="home">
+        <h1>MAP</h1>
+        <button onClick={this.clickHandler}>Explore</button>
+        {rooms}
+        <p>{this.state.localStorage}</p>
+      </div>
+    );
+  }
+}
 
 export default Home;
