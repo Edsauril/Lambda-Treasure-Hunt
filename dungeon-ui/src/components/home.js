a471b493103f67ca012f5d30680a60704ac8d3ab
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-02-06 16:45:35 -0500

    iteration working with cooldown

--- dungeon-ui/src/components/home.js
+++ dungeon-ui/src/components/home.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
 import axios from "axios";
-const elements = ["one", "two", "three"];
 const auth = "Token 666c863bbf493d100e6c1ad9125d2f6265199310";
-const rooms = [];
-elements.forEach(function(element) {
-  rooms.push(<p>{element}</p>);
-});
+let traversalPath = [];
+let graph = {};
+let visited = [];
+let currentExits = [];
+let currentRoom = "";
+let escape = [];
+let route = "";
+let escapeRoute = "";
+let i = 0;
 
 class Home extends Component {
   constructor(props) {
@@ -54,97 +57,82 @@ class Home extends Component {
     window.setTimeout(() => this.move("n"), 15000);
   };
 
+  traversal = () => {
+    console.log("start");
+    console.log("iter");
+    currentRoom = this.state.currentRoom;
+    currentExits = this.state.exits;
+    console.log("Room: " + currentRoom);
+    console.log("Exits: " + currentExits);
+    if (i > 10) {
+      return console.log("finished");
+    }
+    if (!visited.includes(currentRoom)) {
+      visited.push(currentRoom);
+    }
+    if (!(currentRoom in graph)) {
+      currentExits = this.state.exits;
+    } else {
+      currentExits = graph[currentRoom];
+    }
+    graph[currentRoom] = currentExits;
+    if (route) {
+      if (route === "n") {
+        if (graph[currentRoom].includes("s")) {
+          graph[currentRoom].splice(graph[currentRoom].indexOf("s"), 1);
+        }
+        escape.push("s");
+      }
+      if (route === "e") {
+        if (graph[currentRoom].includes("w")) {
+          graph[currentRoom].splice(graph[currentRoom].indexOf("w"), 1);
+        }
+        escape.push("w");
+      }
+      if (route === "s") {
+        if (graph[currentRoom].includes("n")) {
+          graph[currentRoom].splice(graph[currentRoom].indexOf("n"), 1);
+        }
+        escape.push("n");
+      }
+      if (route === "w") {
+        if (graph[currentRoom].includes("e")) {
+          graph[currentRoom].splice(graph[currentRoom].indexOf("e"), 1);
+        }
+        escape.push("e");
+      }
+    }
+    if (graph[currentRoom]) {
+      route =
+        graph[currentRoom][
+          Math.floor(Math.random() * graph[currentRoom].length)
+        ];
+      graph[currentRoom].splice(graph[currentRoom].indexOf(route), 1);
+      traversalPath.push(route);
+      console.log(route);
+      this.move(route);
+    } else {
+      console.log("!!escape!!");
+      escapeRoute = escape.pop();
+      traversalPath.push(escapeRoute);
+      this.move(escapeRoute);
+      route = "";
+    }
+    setTimeout(() => this.traversal(), 30000);
+  };
+
   clickHandler = event => {
     event.preventDefault();
-    localStorage.setItem("message", "we set local storage");
-    let traversalPath = [];
-    let graph = {};
-    let visited = [];
-    let currentExits = [];
-    let currentRoom = "";
-    let escape = [];
-    let route = "";
-    let i = 0;
-    let escapeRoute = "";
-    console.log("start");
-    while (i < 5) {
-      console.log("iter");
-      currentRoom = this.state.currentRoom;
-      currentExits = this.state.exits;
-      console.log("Room: " + currentRoom);
-      console.log("Exits: " + currentExits);
-      if (!visited.includes(currentRoom)) {
-        visited.push(currentRoom);
-      }
-      if (!(currentRoom in graph)) {
-        currentExits = this.state.exits;
-      } else {
-        currentExits = graph[currentRoom];
-      }
-      graph[currentRoom] = currentExits;
-      if (route) {
-        if (route === "n") {
-          if (graph[currentRoom].includes("s")) {
-            graph[currentRoom].splice(graph[currentRoom].indexOf("s"), 1);
-          }
-          escape.push("s");
-        }
-        if (route === "e") {
-          if (graph[currentRoom].includes("w")) {
-            graph[currentRoom].splice(graph[currentRoom].indexOf("w"), 1);
-          }
-          escape.push("w");
-        }
-        if (route === "s") {
-          if (graph[currentRoom].includes("n")) {
-            graph[currentRoom].splice(graph[currentRoom].indexOf("n"), 1);
-          }
-          escape.push("n");
-        }
-        if (route === "w") {
-          if (graph[currentRoom].includes("e")) {
-            graph[currentRoom].splice(graph[currentRoom].indexOf("e"), 1);
-          }
-          escape.push("e");
-        }
-      }
-      if (graph[currentRoom]) {
-        route =
-          graph[currentRoom][
-            Math.floor(Math.random() * graph[currentRoom].length)
-          ];
-        graph[currentRoom].splice(graph[currentRoom].indexOf(route), 1);
-        traversalPath.push(route);
-        console.log(route);
-        setTimeout(() => this.move(route), 30 * 1000);
-      } else {
-        console.log("!!escape!!");
-        escapeRoute = escape.pop();
-        traversalPath.push(escapeRoute);
-        setTimeout(() => this.move(route), 30 * 1000);
-        route = "";
-      }
-
-      console.log(visited.length);
-      i++;
-    }
-
-    // # go through an exit
-    //     if graph[currentRoom]:
-    //         route = random.choice(graph[currentRoom])
-    //         graph[currentRoom].remove(route)
-    //         print(route)
-    //         traversalPath.append(route)
-    //         player.travel(route)
-    //     else:
-    //         print("!!escape!!")
-    //         escapeRoute = (escape.pop())
-    //         print(escapeRoute[-1])
-    //         traversalPath.append(escapeRoute)
-    //         player.travel(escapeRoute)
-    //         route = None
-
-    //     i+=1
+    traversalPath = [];
+    graph = {};
+    visited = [];
+    currentExits = [];
+    currentRoom = "";
+    escape = [];
+    route = "";
+    escapeRoute = "";
+    i = 0;
+    this.traversal();
   };
 
   render() {
@@ -152,7 +140,6 @@ class Home extends Component {
       <div className="home">
         <h1>MAP</h1>
         <button onClick={this.clickHandler}>Explore</button>
-        {rooms}
         <p>{this.state.localStorage}</p>
         <button onClick={this.moveNorth}>Move North</button>
       </div>
